44fddef78595b5f4503b822577114ece72182f8d
Richard E <richardaeld@gmail.com>
2024-10-05 18:23:16 -0400

    beautified

--- php/www/classes/Header.php
+++ php/www/classes/Header.php
@@ -7,16 +7,31 @@ class Header {
       'Contact'
    ];
 
-   static function DisplayNavLinks () :void {
-      ?>
+
+   static function displayHeader () : void { ?>
+<header>
+   <div>
+      Richard Eldridge
+      <span>
+         Software Developer
+      </span>
+   </div>
+   <?=self::displayNavLinks()?>
+   <div>
+      switcher
+   </div>
+</header><?php
+   }
+
+
+   static function DisplayNavLinks () :void { ?>
 <nav>
    <ul>
       <?php foreach (self::NAV_LINKS as $link) { ?>
          <li><a href='#'><?=$link?></a></li>
       <?php } ?>
    </ul>
-</nav>
-      <?php
+</nav><?php
    }
 
 
