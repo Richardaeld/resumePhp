ec6dc131cbea1f9aaf23a71d64cd3a04eaa273dd
Richard E <richardaeld@gmail.com>
2024-10-21 13:31:55 -0400

    Updated greeting cards array

--- php/www/classes/Cards.php
+++ php/www/classes/Cards.php
@@ -7,119 +7,126 @@ class Cards {
    ];
 
    const GREETING_CARDS  = [
-      'Smitten (Hackathon)' => [
-         // 'logo'            => '<span class="smitten">S</span>',
-         'logo'            => '<span class="smitten"><i class="fa-regular fa-heart"></i></span>',
+      'Java From Data Structures' => [
+         'logo'            => 'fa-solid fa-memory',
+         'year'            => '2024',
+         'hackathon'       => 0,
+         'image'           => 'images/multittude-java-projects.jpg',
+         'imageAltText'    => '',
+         'style'           => 'greetingCard',
+         'link'            => 'https://github.com/Richardaeld/Multittude-Java-Projects',
+         'description'     => 'A collection of projects created during Data Structures and Programming II',
+         'descriptionLong' => 'A collection of projects created during Data Structures and Programming II',
+         'stack'           => ['Java'],
+         'repo'            => ['https://github.com/Richardaeld/java-data-structure'],
+         'app'             => '',
+         'awards'          => '',
+      ],
+      'Java From Programming II' => [
+         'logo'            => 'fa-solid fa-memory',
+         'year'            => '2023',
+         'hackathon'       => 0,
+         'image'           => 'images/multittude-java-projects.jpg',
+         'imageAltText'    => '',
+         'style'           => 'greetingCard',
+         'link'            => 'https://github.com/Richardaeld/Multittude-Java-Projects',
+         'description'     => 'A collection of projects created during Data Structures and Programming II',
+         'descriptionLong' => 'A collection of projects created during Data Structures and Programming II',
+         'stack'           => ['Java'],
+         'repo'            => ['https://github.com/Richardaeld/java-programming-2'],
+         'app'             => '',
+         'awards'          => '',
+      ],
+      'Smitten' => [
+         'logo'            => 'fa-regular fa-heart',
+         'year'            => '2022',
+         'hackathon'       => 1,
          'image'           => 'images/smitten1.jpg',
          'imageAltText'    => 'An image of the smitten game being played.',
          'style'           => 'greetingCard',
          'link'            => 'https://www.smittenlove.co.uk/',
          'description'     => '1st place hackathon project at Code Institute\'s, February 2022 Hackathon event.',
          'descriptionLong' => 'This JSON and JavaScript-powered hackathon project achieved 1st place in Code Institute\'s, "Love is in the Air" February 2022 Hackathon event. This application is a Valentine\'s Day themed Wordle, that the team named Smitten.',
-         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'JSON'],
-         'stack'           => 'JavaScript, HTML/CSS, Bootstrap, JSON',
+         'stack'           => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'JSON'],
          'repo'            => ['https://github.com/alissatroiano/8-your-heart-out'],
          'app'             => 'https://alissatroiano.github.io/8-your-heart-out/',
          'awards'          => "1st place in Code Institute's, \"Love is in the Air\" hackathon. ",
       ],
-      'Spinning Wheelchairs (Hackathon)' => [
-         'logo'            => '<span class="spinning-wheelchairs"><i class="fa-solid fa-wheelchair"></i></span>',
+      'Spinning Wheelchairs' => [
+         'logo'            => 'fa-solid fa-wheelchair',
+         'year'            => '2021',
+         'hackathon'       => 1,
          'image'           => 'images/spinning-wheel.jpg',
          'imageAltText'    => 'An image of a spinning wheelchair.',
          'style'           => 'greetingCard',
          'link'            => 'https://richardaeld.github.io/Hackathaon-trust-in-soda-team-2/',
          'description'     => '3rd place hackathon project at code Institutes\'s "Trust in SODA" 2021 event.',
          'descriptionLong' => 'This JavaScript-powered hackathon project achieved 3rd place in the "Trust in SODA" 2021 event in conjunction with Code Institute. This rudimentary project was designed to help determine if a hallway or doorway has adequate space to accommodate a wheelchair\'s wide turn radius.',
-         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'JavaScript, HTML/CSS, Bootstrap',
+         'stack'           => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
          'repo'            => ['https://github.com/Richardaeld/Hackathaon-trust-in-soda-team-2'],
          'app'             => 'https://richardaeld.github.io/Hackathaon-trust-in-soda-team-2/',
          'awards'          => "3rd place in Code Institute's, \"Trust in SODA 2021\" hackathon.",
       ],
-      'Romancing the Cards' => [
-         'logo'            => '<span class="romancing-the-cards"><i class="fa-solid fa-brain"></i></span>',
-         'image'           => 'images/romancing-the-cards.jpg',
-         'imageAltText'    => '',
-         'style'           => 'greetingCard',
-         'link'            => 'https://richardaeld.github.io/google_translate_game/',
-         'description'     => 'A memory matching game with multiple languages and themes.',
-         'descriptionLong' => 'The purpose of this, JavaScript-powered, game is to illustrate how a simple matching game can entertain and help with learning words in a different language. This game boasts 17 different words in 6 languages, 3 different game settings, and 1 cosmetic setting to help encourage returning users. Words for each game are selected at random so each playthrough has non-linear gameplay.',
-         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'JavaScript, HTML/CSS, Bootstrap',
-         'repo'            => ['https://github.com/Richardaeld/google_translate_game'],
-         'app'             => 'https://richardaeld.github.io/google_translate_game/',
-         'awards'          => '',
-      ],
       'DM Tool' => [
-         'logo'            => '<span class="dm-tool"><i class="fa-brands fa-d-and-d"></i></span>',
+         'logo'            => 'fa-brands fa-d-and-d',
+         'year'            => '2022',
+         'hackathon'       => 0,
          'image'           => 'images/dm-tool.jpg',
          'imageAltText'    => '',
          'style'           => 'greetingCard',
          'link'            => 'https://richardaeld.github.io/digital-map/',
          'description'     => 'A project for quick references for D&D 5e',
          'descriptionLong' => 'This PERN project is JavaScript-powered and exists as two separate deployments. The back-end deployment is a RESTful API, and the front-end deployment is a user-facing React.js app. This application is for any Dungeons and Dragons (D&D) 5e fans and can be utilized as a quick way to look up spells, roll digital dice, or view other frequently used table content. As a bonus, the RESTful API allows admins to easily create new tables and expand upon existing content.',
-         'stackArray'      => ['Docker', 'PostgreSQL', 'Knex.js', 'React', 'Express', 'Node', 'JavaScript', 'Bootstrap', 'HTML/CSS'],
-         'stack'           => 'Docker, PostgerSQL, Knex.js, React, Express, Node, JavaScript, Bootstrap, HTML/CSS',
+         'stack'           => ['Docker', 'PostgreSQL', 'Knex.js', 'React', 'Express', 'Node', 'JavaScript', 'Bootstrap', 'HTML/CSS'],
          'repo'            => ['https://github.com/Richardaeld/dm_tool_api', 'https://github.com/Richardaeld/DND_Into_The_Abyss'],
          'app'             => '',
          'awards'          => '',
       ],
       'Fat Raccoon' => [
-         'logo'            => '<span class="fat-raccoon"><i class="fa-solid fa-carrot"></i></span>',
+         'logo'            => 'fa-solid fa-carrot',
+         'year'            => '2021',
+         'hackathon'       => 0,
          'image'           => 'images/fat-raccoon.jpg',
          'imageAltText'    => '',
          'style'           => 'greetingCard',
          'link'            => 'https://richardaeld.github.io/fat-raccoon/',
          'description'     => 'A recipe app that allows users to create, edit, and share recipes.',
          'descriptionLong' => ' The Fat Raccoon was designed to facilitate a home cooking community for the accumulation and sharing of family recipes. This application allows its users to create an account, which will track your previously viewed recipes, favorites, and uploaded recipes. It also allows for multiple ways to search for recipes and when you\'ve selected a recipe you want to cook it provides a cross-out function for the steps and ingredients you\'ve completed and used. This application leverages the flexibility of MongoDB\'s NoSQL database and Flask\'s minimal framework to allow for a truly custom and unique application. ',
-         'stackArray'      => ['Docker', 'Python', 'Flask', 'MongoDB', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'Docker, Python, Flask, MongoDB, JavaScript, HTML/CSS, Bootstrap Github',
+         'stack'           => ['Docker', 'Python', 'Flask', 'MongoDB', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
          'repo'            => ['https://github.com/Richardaeld/Fat_Racoon_Kitchen'],
          'app'             => '',
          'awards'          => 'This project received a Merit for going above and beyond class requirements.',
       ],
       'Eldridge M.B. Emporium' => [
-         'logo'            => '<span class="eldridge-mb-emporium"><i class="fa-solid fa-book"></i></span>',
+         'logo'            => 'fa-solid fa-book',
+         'year'            => '2021',
+         'hackathon'       => 0,
          'image'           => 'images/eldridge-music.jpg',
          'imageAltText'    => 'An image of a Eldridge M.B. Emporium.',
          'style'           => 'greetingCard',
          'link'            => 'https://eldridgembemporium.com/',
          'description'     => 'An ecommerce bookstore app that allows inventory management and secure checkout.',
          'descriptionLong' => ' Eldridge Music Book Emporium is an e-commerce bookstore application that is Django-powered and hosts several interactive functions that are modeled after modern e-commerce sites, such as: notifying users whenever a change has been made to their cart, retaining a purchase history, remembering a user\'s delivery address, and allowing users to rate products. To create a safe and secure checkout method, this application incorporates Stripe\'s API. ',
-         'stackArray'      => ['Docker', 'Python', 'Django', 'PostgreSQL', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'AWS', 'Stripe'],
-         'stack'           => 'Docker, Python, Django, PostgreSQL, JavaScript, HTML/CSS, Bootstrap, AWS, Stripe',
+         'stack'           => ['Docker', 'Python', 'Django', 'PostgreSQL', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'AWS', 'Stripe'],
          'repo'            => ['https://github.com/Richardaeld/Eldridge_Music'],
          'app'             => '',
          'awards'          => 'This project received a Merit for going above and beyond class requirements.',
       ],
-      'Java Projects From Programming II' => [
-         'logo'            => '<span class="multittude-java-projects"><i class="fa-solid fa-memory"></i></span>',
-         'image'           => 'images/multittude-java-projects.jpg',
+      'Romancing the Cards' => [
+         'logo'            => 'fa-solid fa-brain',
+         'year'            => '2021',
+         'hackathon'       => 0,
+         'image'           => 'images/romancing-the-cards.jpg',
          'imageAltText'    => '',
          'style'           => 'greetingCard',
-         'link'            => 'https://github.com/Richardaeld/Multittude-Java-Projects',
-         'description'     => 'A collection of projects created during Data Structures and Programming II',
-         'descriptionLong' => 'A collection of projects created during Data Structures and Programming II',
-         'stackArray'      => ['Java'],
-         'stack'           => 'Java',
-         'repo'            => ['https://github.com/Richardaeld/java-data-structure', 'https://github.com/Richardaeld/java-programming-2'],
-         'app'             => '',
+         'link'            => 'https://richardaeld.github.io/google_translate_game/',
+         'description'     => 'A memory matching game with multiple languages and themes.',
+         'descriptionLong' => 'The purpose of this, JavaScript-powered, game is to illustrate how a simple matching game can entertain and help with learning words in a different language. This game boasts 17 different words in 6 languages, 3 different game settings, and 1 cosmetic setting to help encourage returning users. Words for each game are selected at random so each playthrough has non-linear gameplay.',
+         'stack'           => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
+         'repo'            => ['https://github.com/Richardaeld/google_translate_game'],
+         'app'             => 'https://richardaeld.github.io/google_translate_game/',
          'awards'          => '',
       ],
-      'Java Projects From Data Structures' => [
-         'logo'            => '<span class="multittude-java-projects"><i class="fa-solid fa-memory"></i></span>',
-         'image'           => 'images/multittude-java-projects.jpg',
-         'imageAltText'    => '',
-         'style'           => 'greetingCard',
-         'link'            => 'https://github.com/Richardaeld/Multittude-Java-Projects',
-         'description'     => 'A collection of projects created during Data Structures and Programming II',
-         'descriptionLong' => 'A collection of projects created during Data Structures and Programming II',
-         'stackArray'      => ['Java'],
-         'stack'           => 'Java',
-         'repo'            => ['https://github.com/Richardaeld/java-data-structure', 'https://github.com/Richardaeld/java-programming-2'],
-         'app'             => '',
-         'awards'          => '',
-      ]
    ];
 
 
@@ -129,12 +136,16 @@ class Cards {
    <div class="greeting-card">
       <!-- 1st -->
       <div>
-         <div><div><?=$values['logo']?></div></div>
-         <div><p>
-
-            <?=$name?>
-         </p>
-      </div>
+         <div>
+            <div>
+               <span class="logo">
+                  <i class="<?=$values['logo']?>"></i>
+                  <span><?=$values['year']?></span>
+                  <?php if (!empty($values['hackathon'])) { ?><span>(Hackathon)</span><?php } ?>
+               </span>
+            </div>
+         </div>
+         <div><p><?=$name?></p></div>
       </div>
 
       <!-- 2nd -->
@@ -143,13 +154,11 @@ class Cards {
       <!-- 3rd -->
       <div>
          <div>
-            <!-- <h2>Fully Responsive</h2> -->
             <p>
                <?=$values['description']?><br><br>
-               <b>Stack Used:</b> <br><?=$values['stack']?>
+               <b>Stack:</b> <br><?=implode(', ',$values['stack'])?>
             </p>
          </div>
-         <!-- <p>Get started</p> -->
       </div>
    </div>
 
@@ -159,18 +168,12 @@ class Cards {
    </div>
 
 </div><?php
-// break;
       }
    }
 
 
    static function detailsCard (array $cards) : void { ?>
 <div>
-<span>
-   <i class="fa-solid fa-wheelchair"></i>
-   <i class="fas fa-heart"></i>
-
-</span>
    <div class="details-card-container">
       <div class="details-card">
          <div>
@@ -249,7 +252,7 @@ class Cards {
    }
    function updateStack       (card) {
       const ul = createEl('ul');
-      cards[card]['stackArray'].forEach(stack => {
+      cards[card]['stack'].forEach(stack => {
          const li = createEl('li', {}, stack);
          ul.appendChild(li);
       });
@@ -288,7 +291,7 @@ class Cards {
 
    // ! Dev settings
    document.querySelectorAll('.details-card-container div[data-card-name]')[0].classList.add('selected');
-   updateDetailsCard('Smitten (Hackathon)')
+   updateDetailsCard('Smitten')
    // ! Dev settings
 
    function removeSelected (previousButton) {
