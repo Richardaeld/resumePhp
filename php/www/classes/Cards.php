b6c68395f59a2990781a7ad8b4d53a27782569e5
Richard E <richardaeld@gmail.com>
2024-10-20 21:02:21 -0400

    added wip effect to details-card-links

--- php/www/classes/Cards.php
+++ php/www/classes/Cards.php
@@ -15,8 +15,8 @@ class Cards {
          'link'            => 'https://www.smittenlove.co.uk/',
          'description'     => '1st place hackathon project at Code Institute\'s, February 2022 Hackathon event.',
          'descriptionLong' => 'This JSON and JavaScript-powered hackathon project achieved 1st place in Code Institute\'s, "Love is in the Air" February 2022 Hackathon event. This application is a Valentine\'s Day themed Wordle, that the team named Smitten.',
-         'stackArray'      => ['JavaScript', 'HTML', 'CSS', 'Bootstrap', 'JSON,'],
-         'stack'           => 'JavaScript, HTML, CSS, Bootstrap, JSON',
+         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'JSON'],
+         'stack'           => 'JavaScript, HTML/CSS, Bootstrap, JSON',
          'repo'            => ['https://github.com/alissatroiano/8-your-heart-out'],
          'app'             => 'https://alissatroiano.github.io/8-your-heart-out/',
          'awards'          => "1st place in Code Institute's, \"Love is in the Air\" hackathon. ",
@@ -29,8 +29,8 @@ class Cards {
          'link'            => 'https://richardaeld.github.io/Hackathaon-trust-in-soda-team-2/',
          'description'     => '3rd place hackathon project at code Institutes\'s "Trust in SODA" 2021 event.',
          'descriptionLong' => 'This JavaScript-powered hackathon project achieved 3rd place in the "Trust in SODA" 2021 event in conjunction with Code Institute. This rudimentary project was designed to help determine if a hallway or doorway has adequate space to accommodate a wheelchair\'s wide turn radius.',
-         'stackArray'      => ['JavaScript', 'HTML', 'CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'JavaScript, HTML, CSS, Bootstrap',
+         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
+         'stack'           => 'JavaScript, HTML/CSS, Bootstrap',
          'repo'            => ['https://github.com/Richardaeld/Hackathaon-trust-in-soda-team-2'],
          'app'             => 'https://richardaeld.github.io/Hackathaon-trust-in-soda-team-2/',
          'awards'          => "3rd place in Code Institute's, \"Trust in SODA 2021\" hackathon.",
@@ -43,8 +43,8 @@ class Cards {
          'link'            => 'https://richardaeld.github.io/google_translate_game/',
          'description'     => 'A memory matching game with multiple languages and themes.',
          'descriptionLong' => 'The purpose of this, JavaScript-powered, game is to illustrate how a simple matching game can entertain and help with learning words in a different language. This game boasts 17 different words in 6 languages, 3 different game settings, and 1 cosmetic setting to help encourage returning users. Words for each game are selected at random so each playthrough has non-linear gameplay.',
-         'stackArray'      => ['JavaScript', 'HTML', 'CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'JavaScript, HTML, CSS, Bootstrap',
+         'stackArray'      => ['JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
+         'stack'           => 'JavaScript, HTML/CSS, Bootstrap',
          'repo'            => ['https://github.com/Richardaeld/google_translate_game'],
          'app'             => 'https://richardaeld.github.io/google_translate_game/',
          'awards'          => '',
@@ -57,8 +57,8 @@ class Cards {
          'link'            => 'https://richardaeld.github.io/digital-map/',
          'description'     => 'A project for quick references for D&D 5e',
          'descriptionLong' => 'This PERN project is JavaScript-powered and exists as two separate deployments. The back-end deployment is a RESTful API, and the front-end deployment is a user-facing React.js app. This application is for any Dungeons and Dragons (D&D) 5e fans and can be utilized as a quick way to look up spells, roll digital dice, or view other frequently used table content. As a bonus, the RESTful API allows admins to easily create new tables and expand upon existing content.',
-         'stackArray'      => ['Docker', 'PostgreSQL', 'Knex.js', 'React', 'Express', 'Node', 'JavaScript', 'Bootstrap', 'HTML', 'CSS'],
-         'stack'           => 'Docker, PostgerSQL, Knex.js, React, Express, Node, JavaScript, Bootstrap, HTML, CSS',
+         'stackArray'      => ['Docker', 'PostgreSQL', 'Knex.js', 'React', 'Express', 'Node', 'JavaScript', 'Bootstrap', 'HTML/CSS'],
+         'stack'           => 'Docker, PostgerSQL, Knex.js, React, Express, Node, JavaScript, Bootstrap, HTML/CSS',
          'repo'            => ['https://github.com/Richardaeld/dm_tool_api', 'https://github.com/Richardaeld/DND_Into_The_Abyss'],
          'app'             => '',
          'awards'          => '',
@@ -71,8 +71,8 @@ class Cards {
          'link'            => 'https://richardaeld.github.io/fat-raccoon/',
          'description'     => 'A recipe app that allows users to create, edit, and share recipes.',
          'descriptionLong' => ' The Fat Raccoon was designed to facilitate a home cooking community for the accumulation and sharing of family recipes. This application allows its users to create an account, which will track your previously viewed recipes, favorites, and uploaded recipes. It also allows for multiple ways to search for recipes and when you\'ve selected a recipe you want to cook it provides a cross-out function for the steps and ingredients you\'ve completed and used. This application leverages the flexibility of MongoDB\'s NoSQL database and Flask\'s minimal framework to allow for a truly custom and unique application. ',
-         'stackArray'      => ['Docker', 'Python', 'Flask', 'MongoDB', 'JavaScript', 'HTML', 'CSS', 'Bootstrap', 'Github'],
-         'stack'           => 'Docker Python, Flask, MongoDB, JavaScript, HTML, CSS, Bootstrap Github',
+         'stackArray'      => ['Docker', 'Python', 'Flask', 'MongoDB', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
+         'stack'           => 'Docker, Python, Flask, MongoDB, JavaScript, HTML/CSS, Bootstrap Github',
          'repo'            => ['https://github.com/Richardaeld/Fat_Racoon_Kitchen'],
          'app'             => '',
          'awards'          => 'This project received a Merit for going above and beyond class requirements.',
@@ -85,8 +85,8 @@ class Cards {
          'link'            => 'https://eldridgembemporium.com/',
          'description'     => 'An ecommerce bookstore app that allows inventory management and secure checkout.',
          'descriptionLong' => ' Eldridge Music Book Emporium is an e-commerce bookstore application that is Django-powered and hosts several interactive functions that are modeled after modern e-commerce sites, such as: notifying users whenever a change has been made to their cart, retaining a purchase history, remembering a user\'s delivery address, and allowing users to rate products. To create a safe and secure checkout method, this application incorporates Stripe\'s API. ',
-         'stackArray'      => ['Docker', 'Python', 'Django', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS', 'Bootstrap', 'AWS', 'Stripe'],
-         'stack'           => 'Docker, Python, Django, PostgreSQL, JavaScript, HTML, CSS, Bootstrap, AWS, Stripe',
+         'stackArray'      => ['Docker', 'Python', 'Django', 'PostgreSQL', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'AWS', 'Stripe'],
+         'stack'           => 'Docker, Python, Django, PostgreSQL, JavaScript, HTML/CSS, Bootstrap, AWS, Stripe',
          'repo'            => ['https://github.com/Richardaeld/Eldridge_Music'],
          'app'             => '',
          'awards'          => 'This project received a Merit for going above and beyond class requirements.',
@@ -150,42 +150,46 @@ break;
 
 
    static function detailsCard (array $cards) : void { ?>
-<div class="details-card-container">
-   <div class="details-card">
-      <div>
-         <h3>name</h3>
+<div>
+   <div class="details-card-container">
+      <div class="details-card">
          <div>
-            <img src="" alt="">
+            <h3 class="header-text">name</h3>
             <div>
+               <img src="" alt="">
                <div>
-                  <!-- <span>logo</span> -->
+                  <div>
+                     <!-- <span>logo</span> -->
+                  </div>
+                  <div>
+                     <p>
+                        <span></span>
+                     </p>
+                     <p>
+                        <b>Awards</b><br />
+                        <span></span>
+                     </p>
+                     <p>
+                        <b>Stack Used</b><br/>
+                        <span></span>
+                     </p>
+                  </div>
                </div>
-               <div>
-                  <p>
-                     <span></span>
-                  </p>
-                  <p>
-                     <b>Awards</b><br />
-                     <span></span>
-                  </p>
-                  <p>
-                     <b>Stack Used</b><br/>
-                     <span></span>
-                  </p>
-               </div>
-               <div><span class="fancy-link"><a href="<?=$values['app']?>">App</a></span></div>
             </div>
          </div>
       </div>
+      <div>
+         <!-- project buttons -->
+         <?php foreach ($cards as $name => $values) {?>
+            <div data-card-name="<?=$name?>"><?=$name?></div>
+         <?php } ?>
+      </div>
    </div>
-
-   <div>
-      <?php foreach ($cards as $name => $values) {?>
-         <div data-card-name="<?=$name?>"><?=$name?></div>
-      <?php } ?>
+   <!-- project links -->
+   <div class="details-card-links">
+      <span class="fancy-link"><a href="<?=$values['app']?>">App</a></span>
    </div>
 </div>
-
 <script>
    const
       cards             = <?=json_encode(self::GREETING_CARDS)?>,
@@ -214,8 +218,6 @@ break;
 
    // @ Update the details card
    function updateName        (card) { target.querySelector('h3').textContent            = card;                          }
-   function updateApp         (card) { target.querySelector('div a').href                = cards[card]['app'];            }
-   function updateRepo        (card) { target.querySelector('div span a').href           = cards[card]['repo'][0];        }
    function updateLogo        (card) { target.querySelector('div img').src               = cards[card]['image'];          }
    function updateDescription (card) { target.querySelector('p:first-child').textContent = cards[card]['descriptionLong'];}
    function updateAwards      (card) {
@@ -232,11 +234,30 @@ break;
       target.querySelector('p:last-child span').innerHTML = '';
       target.querySelector('p:last-child span').appendChild(ul);
    }
+   function updateLinks (card) {
+      const linkTarget = document.querySelector('.details-card-links');
+      linkTarget.innerHTML = '';
+
+      // @ Repo(s)
+      Array.from(cards[card]['repo']).forEach(repo => {
+         const span = createEl('span', { class: 'fancy-link' });
+         const a = createEl('a', { href: repo });
+         a.textContent = 'Repo';
+         span.appendChild(a);
+         linkTarget.appendChild(span);
+      })
+
+      // @ App
+      const span = createEl('span', { class: 'fancy-link' });
+      const a = createEl('a', { href: cards[card]['app'] });
+      a.textContent = 'App';
+      span.appendChild(a);
+      linkTarget.appendChild(span);
+   }
    function updateDetailsCard (card) {
       updateName(card);
       updateDescription(card);
-      updateApp(card);
-      updateRepo(card);
+      updateLinks(card);
       updateStack(card);
       updateLogo(card);
       updateAwards(card);
@@ -259,7 +280,8 @@ break;
 
          button.classList.add('selected');
          setTimeout(() => {
-            target.querySelector('.details-card > div').classList.add('animate');
+            target.querySelector('.details-card > div').classList.add('transitionWipe');
+            document.querySelector('.details-card-links').classList.add('transitionWipe');
          }, 1);
 
          setTimeout(() => {
@@ -267,7 +289,8 @@ break;
          }, 500);
 
          setTimeout(() => {
-            target.querySelector('.details-card > div').classList.remove('animate');
+            target.querySelector('.details-card > div').classList.remove('transitionWipe');
+            document.querySelector('.details-card-links').classList.remove('transitionWipe');
          }, 1000);
       });
    });
